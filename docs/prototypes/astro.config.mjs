efb61d4ef99ff0726b455fc63d84914685174ed3
aiiddqd <1852897+aiiddqd@users.noreply.github.com>
2026-05-07 07:41:50 +0300

    fix: remove environment variables from deploy job in GitHub Actions

--- docs/prototypes/astro.config.mjs
+++ docs/prototypes/astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://aiiddqd.github.io',
+	base: '/mfoto',
 	vite: {
 		plugins: [tailwindcss()],
 	},
